0b352cd2e59c22a211644ae16e10e17642a38a2a
william.depaula.agi <william.depaula@agilecontent.com>
2025-10-17 20:38:28 -0300

    Implementing CI

--- SwiftHub/Modules/Home/Package.swift
+++ SwiftHub/Modules/Home/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "Core", path: "Core")
+        .package(name: "Core", path: "../Core")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
