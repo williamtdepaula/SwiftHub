010e8b07bf6c95cc5e371179af9360e117733b28
william.depaula.agi <william.depaula@agilecontent.com>
2025-10-17 11:58:31 -0300

    Initial Commit

---
+++ SwiftHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftHub
+//
+//  Created by Willian de Paula on 17/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftHubTests/SwiftHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHubTests.swift
+//  SwiftHubTests
+//
+//  Created by Willian de Paula on 17/10/25.
+//
+
+import Testing
+@testable import SwiftHub
+
+struct SwiftHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
