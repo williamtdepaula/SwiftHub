351a02c127161253476bade151c5856c9f623ab8
william.depaula.agi <william.depaula@agilecontent.com>
2025-10-19 22:32:43 -0300

    Home layout done

--- SwiftHub/Modules/Home/Sources/Home/View/Components/RepositoryTableViewCell.swift
+++ SwiftHub/Modules/Home/Sources/Home/View/Components/RepositoryTableViewCell.swift
@@ -13,6 +13,10 @@ import Kingfisher
 
 final class RepositoryTableViewCell: UITableViewCell {
     
+    private let MARGIN_EDGE = 16.0
+    
+    private var titleLeadingConstraint: NSLayoutConstraint?
+    
     private lazy var title: UILabel = {
         let view = UILabel()
         view.useConstraints()
@@ -26,7 +30,7 @@ final class RepositoryTableViewCell: UITableViewCell {
         let view = UILabel()
         view.useConstraints()
         view.textColor = Theme.Color.commonText
-        view.font = .systemFont(ofSize: 16, weight: .black)
+        view.font = .systemFont(ofSize: 16, weight: .semibold)
         view.numberOfLines = 2
         return view
     }()
@@ -34,6 +38,49 @@ final class RepositoryTableViewCell: UITableViewCell {
     private lazy var titleImageView: UIImageView = {
         let view = UIImageView()
         view.useConstraints()
+        return view
+    }()
+    
+    private lazy var starIcon: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "Star")
+        view.tintColor = .systemGray
+        view.useConstraints()
+        return view
+    }()
+    
+    private lazy var starsCountLabel: UILabel = {
+        let view = UILabel()
+        view.useConstraints()
+        view.textColor = .systemGray
+        view.font = .systemFont(ofSize: 16, weight: .semibold)
+        view.numberOfLines = 1
+        return view
+    }()
+    
+    private lazy var forkIcon: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "Fork")
+        view.tintColor = .systemGray
+        view.useConstraints()
+        return view
+    }()
+    
+    private lazy var forksLabel: UILabel = {
+        let view = UILabel()
+        view.useConstraints()
+        view.textColor = .systemGray
+        view.font = .systemFont(ofSize: 16, weight: .semibold)
+        view.numberOfLines = 1
+        return view
+    }()
+    
+    private lazy var userNameLabel: UILabel = {
+        let view = UILabel()
+        view.useConstraints()
+        view.textColor = .systemGray
+        view.font = .systemFont(ofSize: 16, weight: .semibold)
+        view.numberOfLines = 1
         return view
     }()
     
@@ -63,16 +110,39 @@ extension RepositoryTableViewCell {
     private func setupDataView(_ data: RepositoryPresentation) {
         title.text = data.title
         descriptionView.text = data.description
+        starsCountLabel.text = data.starsCount
+        forksLabel.text = data.forksCount
+        userNameLabel.text = data.owenerUserName
         setupImage(data)
     }
     
     private func setupImage(_ data: RepositoryPresentation) {
         let roundCorner = RoundCornerImageProcessor(radius: .widthFraction(0.2), roundingCorners: [.all])
         let pngSerializer = FormatIndicatedCacheSerializer.png
-        imageView?.kf.setImage(
+        
+        titleImageView.kf.setImage(
             with: data.owenerAvatarUrl,
             options: [.processor(roundCorner), .cacheSerializer(pngSerializer)]
-        )
+        ) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(_):
+                handleImageTitleLeadingAnchor(isImageLoaded: true)
+            case .failure(_): // Case image fails to load, it sets the leading anchor to leading of cell
+                handleImageTitleLeadingAnchor(isImageLoaded: false)
+            }
+            layoutIfNeeded()
+        }
+    }
+    
+    private func handleImageTitleLeadingAnchor(isImageLoaded: Bool) {
+        self.titleLeadingConstraint?.isActive = false
+        if isImageLoaded {
+            self.titleLeadingConstraint = self.title.leadingAnchor.constraint(equalTo: self.titleImageView.trailingAnchor, constant: 4)
+        } else {
+            self.titleLeadingConstraint = self.title.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: MARGIN_EDGE)
+        }
+        self.titleLeadingConstraint?.isActive = true
     }
 }
 
@@ -82,39 +152,71 @@ extension RepositoryTableViewCell: CodeView {
         contentView.addSubview(titleImageView)
         contentView.addSubview(title)
         contentView.addSubview(descriptionView)
+        
+        contentView.addSubview(starIcon)
+        contentView.addSubview(starsCountLabel)
+        
+        contentView.addSubview(forkIcon)
+        contentView.addSubview(forksLabel)
+        
+        contentView.addSubview(userNameLabel)
     }
     
     func setupConstraints() {
-        let edgeMargin = 16.0
         
         NSLayoutConstraint.activate([
-            titleImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: edgeMargin),
-            titleImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: edgeMargin),
-            titleImageView.widthAnchor.constraint(equalToConstant: 32),
-            titleImageView.heightAnchor.constraint(equalToConstant: 32)
+            titleImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: MARGIN_EDGE),
+            titleImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: MARGIN_EDGE),
+            titleImageView.widthAnchor.constraint(equalToConstant: 24),
+            titleImageView.heightAnchor.constraint(equalToConstant: 24)
+        ])
+        
+        handleImageTitleLeadingAnchor(isImageLoaded: true)
+        
+        NSLayoutConstraint.activate([
+            title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -MARGIN_EDGE),
+            title.centerYAnchor.constraint(equalTo: titleImageView.centerYAnchor)
         ])
         
         NSLayoutConstraint.activate([
-            title.leadingAnchor.constraint(equalTo: titleImageView.trailingAnchor, constant: 4),
-            title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -edgeMargin),
-            title.topAnchor.constraint(equalTo: titleImageView.topAnchor)
+            descriptionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: MARGIN_EDGE),
+            descriptionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -MARGIN_EDGE),
+            descriptionView.topAnchor.constraint(equalTo: titleImageView.bottomAnchor, constant: 6),
+            descriptionView.bottomAnchor.constraint(equalTo: starIcon.topAnchor, constant: -12)
         ])
         
         NSLayoutConstraint.activate([
-            descriptionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: edgeMargin),
-            descriptionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -edgeMargin),
-            descriptionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -edgeMargin),
-            descriptionView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 4)
+            starIcon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: MARGIN_EDGE),
+            starIcon.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -MARGIN_EDGE),
+            starIcon.widthAnchor.constraint(equalToConstant: 16),
+            starIcon.heightAnchor.constraint(equalToConstant: 16),
+        ])
+        
+        NSLayoutConstraint.activate([
+            starsCountLabel.leadingAnchor.constraint(equalTo: starIcon.trailingAnchor, constant: 4),
+            starsCountLabel.bottomAnchor.constraint(equalTo: starIcon.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            forkIcon.leadingAnchor.constraint(equalTo: starsCountLabel.trailingAnchor, constant: 16),
+            forkIcon.bottomAnchor.constraint(equalTo: starsCountLabel.bottomAnchor),
+            forkIcon.widthAnchor.constraint(equalToConstant: 16),
+            forkIcon.heightAnchor.constraint(equalToConstant: 16),
+        ])
+        
+        NSLayoutConstraint.activate([
+            forksLabel.leadingAnchor.constraint(equalTo: forkIcon.trailingAnchor, constant: 4),
+            forksLabel.bottomAnchor.constraint(equalTo: forkIcon.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            userNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -MARGIN_EDGE),
+            userNameLabel.bottomAnchor.constraint(equalTo: forksLabel.bottomAnchor)
         ])
     }
     
     func setupAddtionalConfigs() {
-        contentView.backgroundColor = .clear
+        contentView.backgroundColor = Theme.Color.background
     }
     
-}
-
-// MARK: Constants
-extension RepositoryTableViewCell {
-    static let height: CGFloat = 200
 }
